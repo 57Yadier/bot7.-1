184c397dcb4c057ba75a493ad5eaba2368e14393
57Yadier <103086948+57Yadier@users.noreply.github.com>
2022-06-08 07:55:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ def onmessage(update,bot:ObigramClient):
     try:
         thread = bot.this_thread
         username = update.message.sender.username
-        tl_admin_user = os.environ.get('tl_admin_user')
+        tl_admin_user = os.environ.get('countryandlifee')
 
         #set in debug
         tl_admin_user = 'countryandlifee'
@@ -546,7 +546,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
             #    api_id = 7386053
             #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    bot_token = '5416641824:AAEzeDxBMvwkf-lnZjIPmrTs9LSt7rMW7EE'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
