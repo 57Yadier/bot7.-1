a036d53bd278925b688487bfe70e1d6a407117ef
57Yadier <103086948+57Yadier@users.noreply.github.com>
2022-06-08 12:43:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -541,7 +541,7 @@ def onmessage(update,bot:ObigramClient):
             #if update:
             #    api_id = os.environ.get('api_id')
             #    api_hash = os.environ.get('api_hash')
-            #    bot_token = os.environ.get('bot_token')
+            #    bot_token = os.environ.get('5416641824:AAEzeDxBMvwkf-lnZjIPmrTs9LSt7rMW7EE')
             #    
                 # set in debug
             #    api_id = 7386053
